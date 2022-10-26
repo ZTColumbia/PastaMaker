f38ac36606152859b14b5d2adcdd6472e280a65a
Tong Zhan <tzhan@Tongs-MacBook-Air-2.local>
2022-10-25 20:37:54 -0400

    [add] homepage

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+import flask
+import json
+
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def init():
+    return flask.redirect(url_for('welcome'))
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
 
 
 if __name__ == '__main__':
